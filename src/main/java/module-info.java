e7ae04471be5f5fc84b0928395fc3212ff4e7dc0
hungdungn47 <hungdungn47@gmail.com>
2023-09-29 20:20:29 +0700

    create project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.englishapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.englishapp to javafx.fxml;
+    exports com.example.englishapp;
+}
