03152097095056bcf77f63df39b3d8a3a2ac63a1
hungdungn47 <hungdungn47@gmail.com>
2023-10-04 22:07:31 +0700

    dictionary api

---
+++ src/main/java/com/example/englishapp/Api.java
@@ -0,0 +1,52 @@
+package com.example.englishapp;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.Scanner;
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+
+public class Api {
+    public static void main(String[] args) {
+        String word = "handsome";
+        try {
+            URL url = new URL("https://api.dictionaryapi.dev/api/v2/entries/en/" + word);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.connect();
+
+            int responseCode = con.getResponseCode();
+
+            if(responseCode != 200) {
+                throw new RuntimeException("HttpResponseCode" + responseCode);
+            } else {
+                StringBuilder information = new StringBuilder();
+                Scanner in = new Scanner(url.openStream());
+
+                while(in.hasNext()) {
+                    information.append(in.nextLine());
+                }
+                in.close();
+
+                JSONParser parse = new JSONParser();
+                JSONArray dataObject = (JSONArray) parse.parse(String.valueOf(information));
+
+                JSONObject wordData = (JSONObject) dataObject.get(0);
+                JSONArray meanings = (JSONArray) wordData.get("meanings");
+
+                for(int i = 0; i < meanings.size(); i++) {
+                    JSONObject meaning = (JSONObject) meanings.get(i);
+                    JSONArray defs = (JSONArray) meaning.get("definitions");
+                    JSONObject def = (JSONObject) defs.get(0);
+                    String definition = (String) def.get("definition");
+                    System.out.println("Definition " + i + ": " + definition);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
