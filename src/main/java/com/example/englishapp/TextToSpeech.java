b5e0b7795c53e58b6c2756f29b66ec40d43cdab9
hungdungn47 <hungdungn47@gmail.com>
2023-11-01 23:36:37 +0700

    remove jsapi

--- src/main/java/com/example/englishapp/TextToSpeech.java
+++ src/main/java/com/example/englishapp/TextToSpeech.java
@@ -12,52 +12,8 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.List;
 import java.util.Locale;
-import javax.speech.Central;
-import javax.speech.synthesis.Synthesizer;
-import javax.speech.synthesis.SynthesizerModeDesc;
-import com.sun.speech.freetts.Voice;
-import com.sun.speech.freetts.VoiceManager;
 
 public class TextToSpeech {
-    public static void pronounceUsingJsApi(String word) {
-        try {
-            // Set property as Kevin Dictionary
-            System.setProperty(
-                    "freetts.voices",
-                    "com.sun.speech.freetts.en.us"
-                            + ".cmu_us_kal.KevinVoiceDirectory");
-
-            // Register Engine
-            Central.registerEngineCentral(
-                    "com.sun.speech.freetts"
-                            + ".jsapi.FreeTTSEngineCentral");
-
-            // Create a Synthesizer
-            Synthesizer synthesizer
-                    = Central.createSynthesizer(
-                    new SynthesizerModeDesc(Locale.US));
-
-            // Allocate synthesizer
-            synthesizer.allocate();
-
-            // Resume Synthesizer
-            synthesizer.resume();
-
-            // Speaks the given text
-            // until the queue is empty.
-            synthesizer.speakPlainText(
-                    word, null);
-            synthesizer.waitEngineState(
-                    Synthesizer.QUEUE_EMPTY);
-
-            // Deallocate the Synthesizer.
-//            synthesizer.deallocate();
-        }
-
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
     public static void pronounce(String word) {
         String[] words = word.split(" ");
         try{
